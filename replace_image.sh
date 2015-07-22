0ac020ed7d105854e5cbb66851295d2ba7d43cae
Diego Valle-Jones <diegovalle@gmail.com>
2015-07-21 23:10:45 -0500

    june data

--- replace_image.sh
+++ replace_image.sh
@@ -1,5 +1,5 @@
-find en -type f -print0 | xargs -0 sed -i 's/infographic_apr_2015\.png/infographic_may_2015\.png/g'
-find en -type f -print0 | xargs -0 sed -i 's/municipios_apr_2015\.png/municipios_may_2015\.png/g'
+find en -type f -print0 | xargs -0 sed -i 's/infographic_may_2015\.png/infographic_jun_2015\.png/g'
+find en -type f -print0 | xargs -0 sed -i 's/municipios_may_2015\.png/municipios_jun_2015\.png/g'
 
-find es -type f -print0 | xargs -0 sed -i 's/infographic_es_abr_2015\.png/infographic_es_may_2015\.png/g'
-find es -type f -print0 | xargs -0 sed -i 's/municipios_es_abr_2015\.png/municipios_es_may_2015.png/g'
+find es -type f -print0 | xargs -0 sed -i 's/infographic_es_may_2015\.png/infographic_es_jun_2015\.png/g'
+find es -type f -print0 | xargs -0 sed -i 's/municipios_es_may_2015\.png/municipios_es_jun_2015.png/g'
