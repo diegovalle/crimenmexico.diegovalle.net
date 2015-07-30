58765114b60f9fac005a8c29f720e869e9e99483
Diego Valle-Jones <diegovalle@gmail.com>
2015-07-30 17:40:23 -0500

    something

--- replace_image.sh
+++
@@ -1,5 +0,0 @@
-find en -type f -print0 | xargs -0 sed -i 's/infographic_may_2015\.png/infographic_jun_2015\.png/g'
-find en -type f -print0 | xargs -0 sed -i 's/municipios_may_2015\.png/municipios_jun_2015\.png/g'
-
-find es -type f -print0 | xargs -0 sed -i 's/infographic_es_may_2015\.png/infographic_es_jun_2015\.png/g'
-find es -type f -print0 | xargs -0 sed -i 's/municipios_es_may_2015\.png/municipios_es_jun_2015.png/g'
